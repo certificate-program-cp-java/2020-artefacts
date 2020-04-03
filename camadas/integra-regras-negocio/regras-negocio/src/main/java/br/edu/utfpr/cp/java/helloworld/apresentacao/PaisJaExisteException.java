4cf92a61f11204f4a6c4a3acef5d54133b44aa82
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2020-04-03 17:49:47 -0300

    - Fixed layered projects

--- camadas/integra-regras-negocio/regras-negocio/src/main/java/br/edu/utfpr/cp/java/helloworld/apresentacao/PaisJaExisteException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.utfpr.cp.java.helloworld.apresentacao;
-
-public class PaisJaExisteException extends Exception { 
-
-    public PaisJaExisteException() {
-        super("Já existe um país com esse nome!");
-    }
-}
